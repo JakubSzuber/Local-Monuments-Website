f9268a424cbe99c99b3e670815c16ee063ce28c6
Jakub Szuber <jszuber06@gmail.com>
2022-12-29 01:28:12 +0100

    Added content to docker-entrypoint.sh

--- services/WSGI-server/docker-entrypoint.sh
+++ services/WSGI-server/docker-entrypoint.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 set -e
 
-echo "test zadzialal!!!!!!!!"
+ERROR_NO_APP_WORKDIR=101
+RIGHT_WORKDIR=/app
 
-if ! _is_sourced; then
-	_main "$@"
+function description(){
+  echo -e "Below some information about this container (wsgi gunicorn server) that is core for application work:\n"
+  echo "List of objects in your $RIGHT_WORKDIR working directory:"
+  ls .
+  echo "Tree structure of $RIGHT_WORKDIR working directory::"
+  tree .
+  echo "List of pip packages used for this app:"
+  pip list
+}
+
+if [ ! -d $RIGHT_WORKDIR ]; then
+  echo "ERROR: No right working directory $RIGHT_WORKDIR"
+  echo "xxx.."
+  exit $ERROR_NO_APP_WORKDIR
 fi
+
+description
+
+exec "$@"
