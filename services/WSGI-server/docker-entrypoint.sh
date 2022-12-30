97eaba86b196dd2b7e65eba2bf3162648b083d7d
Jakub Szuber <jszuber06@gmail.com>
2022-12-30 21:34:38 +0100

    Changed lorem ipsum to normal description

--- services/WSGI-server/docker-entrypoint.sh
+++ services/WSGI-server/docker-entrypoint.sh
@@ -7,8 +7,8 @@ RIGHT_WORKDIR=/app
 function description(){
   echo -e "Below some information about this container (wsgi gunicorn server) that is core for application work:\n"
   echo "List of objects in your $RIGHT_WORKDIR working directory:"
-  ls .
-  echo "Tree structure of $RIGHT_WORKDIR working directory::"
+  ls -l .
+  echo "Tree structure of $RIGHT_WORKDIR working directory:"
   tree .
   echo "List of pip packages used for this app:"
   pip list
